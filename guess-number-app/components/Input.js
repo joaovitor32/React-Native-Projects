2e317b01d1801d6d79fd194fd927e9116c581e96
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-03-20 11:46:28 -0300

    Guess-Number-App

---
+++ guess-number-app/components/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {TextInput,StyleSheet} from 'react-native';
+
+const Input = props=>{
+
+    return (
+        <TextInput {...props} style={{...styles.input,...props.style}}/>
+    )
+
+}
+
+const styles=StyleSheet.create({
+    input:{
+        height:30,
+        borderBottomColor:"black",
+        borderBottomWidth:1,
+        marginVertical:20,
+    }
+})
+
+export default Input;
